fd649ef61d28f255fe92e7b5fc8006710253467a
Sanoop <sanoop@finbox.in>
2020-05-22 17:12:13 +0530

    Update SDK integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FinBoxBankConnect } from "finbox-bank-connect-js";
+import React, { useState } from "react";
 
-function App() {
+const App = props => {
+  const [showModal, setShowModal] = useState(false);
+
+  const showBankModal = () => {
+    setShowModal(true);
+  };
+
+  /**
+   * Callback when user exits bankconnect journey
+   */
+  const onExit = () => {
+    setShowModal(false);
+  };
+
+  /**
+   * Callback when any error is encountered during the journey
+   * 
+   * @param payload - contains reason and linkId
+   */
+  const onError = payload => {
+    console.log("Error -> ", payload);
+  };
+
+  /**
+   * Callback when statement has been uploaded to FinBox.
+   * 
+   * @param payload - contains entityId and linkId
+   */
+  const onSuccess = payload => {
+    console.log("Payload -. ", payload);
+    setShowModal(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 640 }}>
+      {showModal && (
+        <FinBoxBankConnect
+          linkId="fnu39-2122-33332-asd2d" //Random UUID
+          apiKey="<api-key>" //API Key provided by FinBox
+          onSuccess={onSuccess}
+          onError={onError}
+          onExit={onExit}
+        />
+      )}
+      <button onClick={showBankModal}>Upload Statement</button>
     </div>
   );
-}
+};
 
 export default App;
